1328ae63be82c969fb3efbc079a8b55e2c201008
SWORNA <imsworna@gmail.com>
2025-07-17 16:30:32 +0545

    added types for Bottom tab navigation

--- App.tsx
+++ App.tsx
@@ -11,19 +11,72 @@ import MenuScreen from './src/screens/MenuScreen';
 import { NavigationContainer } from '@react-navigation/native';
 import MaterialIcons from '@react-native-vector-icons/material-icons';
 
-export type ScreenNames = ["Home", "AddTodo", "Details"]
-export type RootStackParamList = Record<ScreenNames[number], undefined>;
+export type StackParamList = {
+  Home: undefined;
+  AddTodo: undefined;
+  Details: undefined;
+};
 
-const Stack = createNativeStackNavigator<RootStackParamList>();
+export type BottomTabParamList ={
+  HomeTab: undefined,
+  Menu: undefined,
+  Profile: undefined
+}
+
+const Stack = createNativeStackNavigator<StackParamList>();
 const Tab = createBottomTabNavigator();
 
-function HomeStack() {
+const config = {
+  animation: 'spring',
+  config: {
+    stiffness: 1000,
+    damping: 500,
+    mass: 3,
+    overshootClamping: true,
+    restDisplacementThreshold: 0.01,
+    restSpeedThreshold: 0.01,
+  },
+};
+
+function HomeTabs() {
   return (
-    <Stack.Navigator initialRouteName="Home">
-      <Stack.Screen name="Home" component={HomeScreen} />
-      <Stack.Screen name="AddTodo" component={AddTodoScreen} />
-      <Stack.Screen name="Details" component={DetailsScreen} />
-    </Stack.Navigator>
+    <Tab.Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarShowLabel: true,
+        tabBarStyle: { backgroundColor: '#638265ff', height: 70 },
+        tabBarInactiveTintColor: '#dae2da',
+        tabBarActiveTintColor: 'white',
+      }}
+    >
+      <Tab.Screen
+        name="HomeTab"
+        component={HomeScreen}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <MaterialIcons name="home" size={24} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Menu"
+        component={MenuScreen}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <MaterialIcons name="menu" size={24} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={ProfileScreen}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <MaterialIcons name="face-3" size={24} color={color} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
   );
 }
 
@@ -31,44 +84,19 @@ export default function App() {
   return (
     <Provider store={store}>
       <NavigationContainer>
-        <Tab.Navigator 
-        screenOptions={{ 
-          headerShown: false,
-          tabBarShowLabel: true,
-          tabBarStyle:{backgroundColor:'#638265ff', height:70},
-          tabBarInactiveTintColor:"#dae2da",
-          tabBarActiveTintColor:"white",
-
-          }}>
-          <Tab.Screen
-            name="Home"
-            component={HomeStack}
-            options={{
-              tabBarIcon: ({color}) => (
-                <MaterialIcons name="home" size={24} color={color} />
-              )         
-            }}
-          />
-          <Tab.Screen
-            name="Menu"
-            component={MenuScreen}
-            options={{
-              tabBarIcon: ({color}) => (
-                <MaterialIcons name="menu" size={24} color={color} />
-              )
-            }}
-          />
-          <Tab.Screen
-            name="Profile"
-            component={ProfileScreen}
-            options={{
-              tabBarIcon: ({color}) => (
-                <MaterialIcons name="face-3" size={24} color={color} />
-              ),
-            }}
-          />
-        </Tab.Navigator>
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen name="Home" component={HomeTabs} />
+          <Stack.Screen name="AddTodo" component={AddTodoScreen} 
+          options={{
+            transitionSpec:{
+              open: config,
+              close: config
+            }
+          }}/>
+          <Stack.Screen name="Details" component={DetailsScreen} />
+        </Stack.Navigator>
       </NavigationContainer>
     </Provider>
   );
 }
+
